014c26f224023e318e0fed9a114d8088f22d508f
Roleeygit <juhaszroli2001@gmail.com>
2022-06-14 08:28:23 +0200

    Mainframe kész

--- src/views/MainFrame.java
+++ src/views/MainFrame.java
@@ -10,23 +10,65 @@ Licenc: GNU GPL
 
 package views;
 
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-
 import javax.swing.JFrame;
+
 import javax.swing.JPanel;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
 
 public class MainFrame extends JFrame {
     JPanel mainPanel;
+
+    public DefaultComboBoxModel<String> defaultComboBoxModel;
+    public JComboBox<String> positionComboBox;
+
+    public DefaultListModel<String> defaultListModel;
+    public JList<String> positionList;
+    JScrollPane scrollPane;
+
+    JButton exitButton;
+    JButton emptyButton;
+    JPanel buttonPanel;
+
     public MainFrame() {
         setMainComponent();
         setMainPanel();
         setMainFrame();
     }
     private void setMainComponent() {
-        
+        defaultComboBoxModel = new DefaultComboBoxModel<>();
+        positionComboBox = new JComboBox<>(defaultComboBoxModel);    
+        defaultComboBoxModel.addElement("Válasszon beosztást...");    
+        defaultListModel = new DefaultListModel<>();
+        positionList = new JList<>(defaultListModel);
+        scrollPane = new JScrollPane(positionList);
+
+        exitButton = new JButton("Kilépés");
+        emptyButton = new JButton("Ürítés");
+    }
+    private void setButtonPanel() {
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
+        buttonPanel.add(emptyButton);
+        buttonPanel.add(exitButton);
     }
     private void setMainPanel() {
         mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout()); 
+        mainPanel.add(positionComboBox, BorderLayout.NORTH);
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
+
+        setButtonPanel();
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+        this.add(mainPanel);
     }
     private void setMainFrame() {
         this.add(mainPanel);

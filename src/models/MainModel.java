0e029f8e385129f1506881cfeec0cd080bbe7f67
Roleeygit <juhaszroli2001@gmail.com>
2022-06-14 08:19:02 +0200

    Modellek kész

--- src/models/MainModel.java
+++ src/models/MainModel.java
@@ -10,6 +10,22 @@ Licenc: GNU GPL
 
 package models;
 
+import java.util.Arrays;
+import java.util.Vector;
+
 public class MainModel {
-    
+    Position[] positionArray = {
+        new Position(1, "Szoftverfejlesztő"),
+        new Position(2, "Szoftvertesztelő"),
+        new Position(3, "Rendszergazda"),
+        new Position(4, "Tanár"),
+        new Position(5, "Igazgató"),
+        new Position(6, "Titkár"),
+        new Position(7, "Takarító"),
+        new Position(8, "Taxisofőr"),
+        new Position(9, "Kalauz"),
+        new Position(10, "Munkanélküli"),
+    };
+
+    public Vector<Position> positionVector = new Vector<>(Arrays.asList(positionArray));
 }
--- src/models/Position.java
+++ src/models/Position.java
@@ -11,5 +11,11 @@ Licenc: GNU GPL
 package models;
 
 public class Position {
-    
+    public int id;
+    public String name;
+
+    public Position(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
